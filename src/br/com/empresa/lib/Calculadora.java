f33c16f568c27f784826603992f9b72fbd09f1f9
Daniel Walter <danielwalterpand@gmail.com>
2017-08-15 21:30:32 -0300

    Implementação de "Ctrl-Z"

--- src/br/com/empresa/lib/Calculadora.java
+++ src/br/com/empresa/lib/Calculadora.java
@@ -14,8 +14,48 @@ public class Calculadora {
 		return this.resultado;
 	}
 	
-	
 	public double Somar(double valor) {
 		return this.resultado += valor;
 	}
+	
+	public double Somarv(double valor) {
+		this.resultado += valor;
+		return this.resultado -= valor;
+	}
+	
+	public double Subtrair(double valor) {
+		return this.resultado -= valor;
+		
+	}
+	
+	public double Subtrairv(double valor) {
+		 this.resultado -= valor;
+		 return this.resultado += valor;
+		
+	}
+
+
+	public double Dividir(double valor) {
+		return this.resultado /= valor;
+		
+	}
+	
+	public double Dividirv(double valor) {
+		this.resultado /= valor;
+		return this.resultado *= valor;
+		
+	}
+	
+	
+	public double Multiplicar(double valor) {
+		return this.resultado *= valor;
+		
+	}
+	
+	public double Multiplicarv(double valor) {
+		this.resultado *= valor;
+		return this.resultado /= valor;
+		
+	}
+	
 }
--- src/br/com/empresa/test/TesteCalculadora.java
+++ src/br/com/empresa/test/TesteCalculadora.java
@@ -23,13 +23,62 @@ public class TesteCalculadora {
 		calculadora.Somar(7);
 		assertEquals(calculadora.getResultado(), 10, 0);
 
-
+	}
+	
+	@Test
+	public void somaNumerosInteirosEVolta() {
+		calculadora = new Calculadora(10);
+		calculadora.Somarv(3);
+		assertEquals(calculadora.getResultado(), 10, 0);
 	}
 	
 	@Test
 	public void somaNumeroscomVírgula() {
 		calculadora.Somar(2.33);
 		assertEquals(calculadora.getResultado(), 2.33, 0);
+	}
+	
+	@Test
+	public void somaValorEDivide() {
+		calculadora.Somar(20);
+		calculadora.Dividir(2);
+		assertEquals(calculadora.getResultado(), 10, 0);
+	}
+	
+	@Test
+	public void somaValorEDivideEVolta() {
+		calculadora.Somar(20);
+		calculadora.Dividirv(2);
+		assertEquals(calculadora.getResultado(), 20, 0);
+	}
+	
+	@Test
+	public void criaCalculadoraComValorSubtrai() {
+		calculadora = new Calculadora(10);
+		calculadora.Subtrair(5);
+		assertEquals(calculadora.getResultado(), 5, 0);
+	}
+	
+	@Test
+	public void criaCalculadoraComValorSubtraiEVolta() {
+		calculadora = new Calculadora(10);
+		calculadora.Subtrairv(5);
+		assertEquals(calculadora.getResultado(), 10, 0);
+
+	}
+	
+	@Test
+	public void criaCalculadoraComValorMultiplica() {
+		calculadora = new Calculadora(10);
+		calculadora.Multiplicar(5);
+		assertEquals(calculadora.getResultado(), 50, 0);
+	}
+	
+	@Test
+	public void criaCalculadoraComValorMultiplicaEVoltar() {
+		calculadora = new Calculadora(10);
+		calculadora.Multiplicarv(5);
+		assertEquals(calculadora.getResultado(), 10, 0);
 	}
 	
 	@Test
