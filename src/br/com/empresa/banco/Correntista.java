7ea36e8dfd9b91e5b6173dc6f4119c6c533c6846
Daniel Walter <danielwalterpand@gmail.com>
2017-08-29 19:26:55 -0300

    Simulação CONTA

---
+++ src/br/com/empresa/banco/Correntista.java
@@ -0,0 +1,20 @@
+package br.com.empresa.banco;
+
+public class Correntista {
+	private String nome;
+	private String cpf;
+	
+	public Correntista(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public String GetNome() {
+		return this.nome;
+	}
+	
+	public String GetCPF() {
+		return this.cpf;
+	}
+}
+		
